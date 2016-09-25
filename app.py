71562ec75be5f78b6a5c30a9f5aaf0efcb0c8993
Mark Norwich <MarkNorwich@marks-mbp.dhcp.wustl.edu>
2016-09-25 16:26:16 -0500

    resume and contact

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from jinja2 import Template
 
 
 # this makes app an instance of the Flask class
 # and it passed the special variable __name__ into
 # the constructor
+ 
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def help():
-    return render_template("index.html")
+def index():
+	name = request.args.get("name",None)
+	email = request.args.get("email",None)
+	message = request.args.get("message", None)
+	button = request.args.get("b",None)
+	if button == None:
+		return render_template("index.html")
+	else:
+		return render_template("index.html", name=name)
 
 
-if __name__=="__main__":
-    app.run(port=5000) 
+if __name__=="__main__": 
+	app.debug = True
+	app.run(port=5000) 
    
